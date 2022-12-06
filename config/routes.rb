0744bfd462026e889431b7ebbc58d51d98bfe74c
Lazare Boddaert <lazare.boddaert@gmail.com>
2022-12-06 12:45:07 +0100

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,16 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      get 'nations/index'
-      get 'nations/show'
-      get 'nations/create'
+      resources :nations, only: %i[index show create]
+      resources :regions, only: %i[index show create]
+      resources :departements, only: %i[index show create]
+      resources :communes, only: %i[index show create]
+      resources :scrutins, only: %i[index show create]
+      resources :candidats, only: %i[index show create]
+      resources :formation_politiques, only: %i[index show create]
+      resources :courant_politiques, only: %i[index show create]
+      resources :resultat_communes, only: %i[index show create]
+      resources :resultat_candidats, only: %i[index show create]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
